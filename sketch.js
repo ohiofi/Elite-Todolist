d52b1ea1de3d4f3c1ab1c671e974264141b7592a
von9880 <tony.auron.brown@gmail.com>
2026-02-11 18:55:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,3 @@
-let task0 = new Task(); 
-let task1 = new Task(); 
-let task2 = new Task(); 
-
-let list0 = new List("my list");
-list0.addTask(task0);
-list0.addTask(task1);
-list0.addTask(task2);
-
-list0.pushToLocalStorage("1");
-
-list1 = new List("");
-list1.getFromLocalStorage("1");
-console.log(list1)
-
 function setup() {
   createCanvas(400, 400);
 }
