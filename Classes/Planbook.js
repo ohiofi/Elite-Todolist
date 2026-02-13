b3361e7498f4d24c596581b42d10254c3697e947
ohiofi <20hz20khz@gmail.com>
2026-02-13 16:28:40 -0500

    added callbacks

--- Classes/Planbook.js
+++ Classes/Planbook.js
@@ -1,18 +1,25 @@
 class Planbook {
     constructor(){
         // using a dictionary-like structure so that duplicate lists won't happen
-        this.directory = {
-            "Groceries": new List("Groceries"),
-            "Movies To Watch": new List("Movies To Watch"),
-            "Archive": new ArchiveList()
-        }
+        // this.dictionary = {
+        //     "Groceries": new List("Groceries"),
+        //     "Movies To Watch": new List("Movies To Watch"),
+        //     "Archive": new ArchiveList()
+        // }
+        this.dictionary = {};
+
+        this.xLocationForNextList = 10;
+
+        this.addList("Groceries");
+        this.addList("Movies To Watch");
+        this.addArchiveList();
  
         // add some example tasks
-        this.directory["Groceries"].addTask(new Task("Apples", "Get 2 Honeycrisp apples"));
-        this.directory["Groceries"].addTask(new Task("Bananas", "3 or 4 green bananas"));
-        this.directory["Movies To Watch"].addTask(new Task("Marty Supreme", "About table tennis?"));
-        this.directory["Movies To Watch"].addTask(new Task("The Muppet Show", "Seth Rogan is in it"));
-        this.directory["Movies To Watch"].addTask(new Task("F1", "Cars go vroom"));
+        this.dictionary["Groceries"].addTask(new Task("Apples", "Get 2 Honeycrisp apples"));
+        this.dictionary["Groceries"].addTask(new Task("Bananas", "3 or 4 green bananas"));
+        this.dictionary["Movies To Watch"].addTask(new Task("Marty Supreme", "About table tennis?"));
+        this.dictionary["Movies To Watch"].addTask(new Task("The Muppet Show", "Seth Rogan is in it"));
+        this.dictionary["Movies To Watch"].addTask(new Task("F1", "Cars go vroom"));
 
     }
 
@@ -21,19 +28,75 @@ class Planbook {
     }
 
     show(){
-        let x = 10;
-        for (const [key, value] of Object.entries(this.directory)) {
-            value.show(x);
-            x += 410
+        for (const [key, value] of Object.entries(this.dictionary)) {
+            value.show();
         }
     }
 
-    archiveTask(startListObject, currentIndex){
-        // TODO
+    addArchiveList(){
+        // if list name already exists in dictionary, then do nothing
+        if(this.doesListExist("Archive")){ return }
+
+        this.dictionary["Archive"] = new ArchiveList();
+        this.dictionary["Archive"].setX(this.xLocationForNextList)
+        this.xLocationForNextList += 410;
+
+        // connect add of the handler callbacks
+        this.bindCallbackFunctions(this.dictionary["Archive"]);
+    }
+
+    addList(listNameString){
+        // if list name already exists in dictionary, then do nothing
+        if(this.doesListExist(listNameString)){ return }
+
+        this.dictionary[listNameString] = new List(listNameString);
+        this.dictionary[listNameString].setX(this.xLocationForNextList)
+        this.xLocationForNextList += 410;
+
+        // connect add of the handler callbacks
+        this.bindCallbackFunctions(this.dictionary[listNameString]);
+    }
+
+    archiveTask(startListObject, taskObject){
+        console.log("hello from archive task")
+        // copy to archive
+        if(!this.doesListExist("Archive")){
+            this.addArchiveList();
+        }
+        this.dictionary["Archive"].addTask(taskObject);
+        this.dictionary[startListObject.getName()].removeTask(taskObject);
+        taskObject.setArchived()
+    }
+
+    bindCallbackFunctions(listObject){
+        listObject.handleDeleteListPress = () => {
+            this.deleteList(listObject);
+        }
+        listObject.handleMoveTaskToArchive = (taskObject) => {
+            this.archiveTask(listObject, taskObject);
+        }
     }
 
     deleteList(listObjectToDelete){
         // TODO
+        console.log("planbook receive request to delete list")
+
+        let xLocationOfDeletedList = listObjectToDelete.x;
+
+        listObjectToDelete.deleteListButtons();
+
+        delete this.dictionary[listObjectToDelete.getName()];
+
+        // loop thru the existing lists and move if needed
+        for (const [key, value] of Object.entries(this.dictionary)) {
+            if(value.x > xLocationOfDeletedList){
+                value.setX(value.x - 410);
+            }
+        }
+    }
+
+    doesListExist(listNameString){
+        return this.dictionary.hasOwnProperty(listNameString)
     }
 
     findTask(taskNameString){
@@ -57,6 +120,8 @@ class Planbook {
 
     moveTask(startListObject, currentIndex, endingListObject){
         // TODO
+        task.setCompleted();
+        list.moveTask(listArray[0], this);
     }
 
     renameList(listObjectToRename, newNameString){
