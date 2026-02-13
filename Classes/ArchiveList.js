3f769cfbc96e129cd14cd04eb07ec8cebe450325
ohiofi <20hz20khz@gmail.com>
2026-02-13 16:22:05 -0500

    use this.x and this.y

--- Classes/ArchiveList.js
+++ Classes/ArchiveList.js
@@ -4,25 +4,25 @@ class ArchiveList extends List {
     }
 
 
-    show(x) {
+    show() {
         // box
-        rect(x, 10, 400, 1000);
+        rect(this.x, this.y, 400, 1000);
 
 
         // title
         textAlign(CENTER, CENTER);
         fill(0);
-        text(this.name, x + 200, 30);
+        text(this.name, this.x + 200, 30);
         fill(255);
 
 
 
 
         // show all tasks in this list
-        let y = 70;
+
         for (const each of this.listStorage) {
-            each.show(x + 10, y, true);
-            y += 130
+            each.show();
+            
         }
     }
 }
