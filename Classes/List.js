333ea211f405652f30b3521af9aaaccb88b8e18d
von9880 <tony.auron.brown@gmail.com>
2026-02-14 15:20:26 -0500

    Update List.js

--- Classes/List.js
+++ Classes/List.js
@@ -5,6 +5,14 @@ class List{
     constructor(name){
         this.name = name || DEFAULT_LIST_NAME
         this.listStorage = [];
+
+        this.addTaskButton = createButton(`Add Task`);
+        this.addTaskButton.hide();
+        this.addTaskButton.mousePressed(() => this.buttonPressedAddTask());
+
+        this.deleteListButton = createButton(`Delete List`);
+        this.deleteListButton.hide();
+        this.deleteListButton.mousePressed(() => this.buttonPressedDeleteList());
     }
 
     //Getters
@@ -14,6 +22,13 @@ class List{
 
     getName(){
         return this.name;
+    }
+
+
+    getNewTask(){
+        let name =  prompt("Input the task name.");
+        let desc = prompt("Input the tasks description.");
+        return new Task(name, desc);
     }
 
     //Adds Task object to storage in List object.
@@ -47,6 +62,8 @@ class List{
 
         //move to archive
         //todo
+
+       
     }
 
     //swap the first index with the second index
@@ -59,7 +76,7 @@ class List{
     // this will swap the tasks at index and index + 1
     moveDown(index){
         // do a safety check to avoid index out of range
-        if(index >= listStorage.length - 1){
+        if(index >= this.listStorage.length - 1){
             return;
         }
         this.swapIndex(index, index + 1);
@@ -82,7 +99,30 @@ class List{
         this.removeTask(task);
     }
 
+    
 
+    deleteListButtons(){
+        this.addTaskButton.remove()
+        this.deleteListButton.remove();
+    }
+    
+    buttonPressedAddTask(){
+        this.addTask(this.getNewTask())
+        refresh();
+    }
+
+    deleteTaskButtons(){
+        for (let each of this.listStorage) {
+            each.deleteTaskButtons();
+        }
+    }
+
+    buttonPressedDeleteList(){
+        this.deleteListButtons()
+        this.deleteTaskButtons()
+        listArray.splice(listArray.indexOf(this), listArray.indexOf(this)>= 0 ? 1 : 0);
+        refresh();
+    }
 
     toString(){
         let output = `List: ${this.name}\n`;
@@ -103,35 +143,75 @@ class List{
     //will need worked on a bit when we get multiple lists
     pushToLocalStorage(listID){
         //uploads the obj it to local storage under the key name of what ever is stored in listID
-        const stringObj = JSON.stringify(this)
-        localStorage.setItem(listID, stringObj);
+        //const stringObj = JSON.stringify(this) <-- this stopped working when we added the buttons
+
+        //does the same thing but without the buttons
+        const data = {
+            name: this.name,
+            listStorage: this.listStorage
+        };
+        localStorage.setItem(listID, JSON.stringify(data));
+    }
+    
+
+
+getFromLocalStorage(listId){
+    //gets data from local storage
+    const data = localStorage.getItem(listId);
+    if(data === null) return;
+
+    //converts it to be useable agige
+    const parsedData = JSON.parse(data);
+
+    //resets the name
+    this.name = parsedData.name;
+
+    //clears the data
+    this.listStorage = []; 
+
+    //needed to add the tasks to the list stoage and i forgot to do that... mb
+    if (parsedData.listStorage) {
+        for (let item of parsedData.listStorage) {
+            let task = Task.fromJSON(item);
+            this.addTask(task); 
+        }
+    }
+}
+
+    show(x) {
+        // box
+        rect(x, 10, 400, 1000, 15);
+
+        //sets pos of buttons
+        this.addTaskButton.position(x + 10, 20);
+        this.deleteListButton.position(x + 310, 20);
+
+        //shows buttons
+        this.addTaskButton.show();
+        this.deleteListButton.show();
+
+        // title
+        textAlign(CENTER, CENTER);
+        fill(0);
+        text(this.name, x + 200, 30);
+        fill(255);
+
+        // show all tasks in this list
+        if(this.listStorage.length > 0){
+            //console.log("show")
+            this.showTask()
+        }
+        
+        
+    
     }
 
-
-    getFromLocalStorage(listId){
-        //gets data
-        const data = localStorage.getItem(listId);
-
-
-        //fail safe to check if there is data
-        if(data === null){
-            return;
+    showTask(){
+        let y = 70;
+        for (let each of this.listStorage) {
+            each.show(x + 10, y);
+            y += 130;
         }
-
-        //converts data back
-        const parsedData = JSON.parse(data);
-
-        //sets the name of the list to the name saved in local Storage
-        this.name =  parsedData.name;
-
-        //asinged the objs to a class so it regains its methods
-        if (parsedData.listStorage) {
-            this.listStorage = parsedData.listStorage.map(item => Task.fromJSON(item));
-        }
-    }
-
-    show(){
-        //todo
     }
 
 }
